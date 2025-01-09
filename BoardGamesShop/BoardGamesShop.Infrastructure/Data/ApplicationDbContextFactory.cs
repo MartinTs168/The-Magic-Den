61b4b9180795d760d0ac345162ebc16ecba76357
MartinTs168 <martin.tsenev@gmail.com>
2025-01-09 17:18:55 +0200

    changed con string and added ApplicationDbContextFactory for migrations

---
+++ BoardGamesShop/BoardGamesShop.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,24 @@
+using BoardGamesShop.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BoardGamesShop.Infrastructure.Data;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Build configuration from the main project's appsettings.json
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../BoardGamesShop")) // Path to the main project
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Configure DbContext options
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
