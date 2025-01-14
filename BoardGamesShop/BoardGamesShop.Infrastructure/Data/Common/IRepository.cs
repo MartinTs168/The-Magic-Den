0a93c85658d809ef7fc0036bce027fdc30630990
MartinTs168 <martin.tsenev@gmail.com>
2025-01-14 10:15:06 +0200

    implemented IUnit of work pattern
    The files are named Repository and IRepository for the purpose of convinience

---
+++ BoardGamesShop/BoardGamesShop.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,16 @@
+namespace BoardGamesShop.Infrastructure.Data.Common;
+
+public interface IRepository
+{
+    IQueryable<T> All<T>() where T : class;
+
+    IQueryable<T> AllReadOnly<T>() where T : class;
+
+    Task AddAsync<T>(T entity) where T : class;
+    
+    Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+    Task<int> SaveChangesAsync();
+    
+    Task DeleteAsync<T>(object id) where T : class;
+}
