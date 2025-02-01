a6e0a1014bbc5f0c5fe65feb17ee1be058c8d148
MartinTs168 <martin.tsenev@gmail.com>
2025-02-01 16:17:55 +0200

    fix in OrderItem class: added virtual to the nav properties to work with lazy loading

--- BoardGamesShop/BoardGamesShop.Infrastructure/Data/Entities/OrderItem.cs
+++ BoardGamesShop/BoardGamesShop.Infrastructure/Data/Entities/OrderItem.cs
@@ -8,14 +8,14 @@ public class OrderItem
     public int OrderId { get; set; }
     
     [ForeignKey(nameof(OrderId))]
-    public Order? Order { get; set; }
+    public virtual Order? Order { get; set; }
     
     [Required]
     public int GameId { get; set; }
 
     [Required]
     [ForeignKey(nameof(OrderId))]
-    public Game Game { get; set; } = null!;
+    public virtual Game Game { get; set; } = null!;
     
     [Required]
     public int Quantity { get; set; }
