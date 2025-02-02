044b86ab7a576ac91e0adb5325d5bf9cde78ee4c
MartinTs168 <martin.tsenev@gmail.com>
2025-02-02 22:13:23 +0200

    fix to ClaimsPrincipalExtensions.cs

--- BoardGamesShop/BoardGamesShop/Extensions/ClaimsPrincipalExtensions.cs
+++ BoardGamesShop/BoardGamesShop/Extensions/ClaimsPrincipalExtensions.cs
@@ -2,13 +2,10 @@ namespace System.Security.Claims;
 
 public static class ClaimsPrincipalExtensions
 {
-    public static Guid? Id(this ClaimsPrincipal user)
+    public static Guid Id(this ClaimsPrincipal user)
     {
-        if (Guid.TryParse(user.FindFirstValue(ClaimTypes.NameIdentifier), out var userId))
-        {
-            return userId;
-        }
-
-        return null;
+        var id = Guid.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier));
+        
+        return id;
     }
 }
