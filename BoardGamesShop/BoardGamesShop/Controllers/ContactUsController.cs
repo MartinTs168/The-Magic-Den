d9d2cf8ea485473984afa7904de9306cf97befb5
MartinTs168 <martin.tsenev@gmail.com>
2025-03-17 09:59:54 +0200

    made the contacts view

--- BoardGamesShop/BoardGamesShop/Controllers/ContactUsController.cs
+++ BoardGamesShop/BoardGamesShop/Controllers/ContactUsController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoardGamesShop.Controllers;
 
 public class ContactUsController : BaseController
 {
+    [AllowAnonymous]
     public IActionResult Index()
     {
         return View();
