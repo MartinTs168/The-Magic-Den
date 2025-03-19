09e6954d9fc92f0aedef55bfff8f1dad675d4c5d
MartinTs168 <martin.tsenev@gmail.com>
2025-03-19 09:57:49 +0200

    moved the events of the pagination buttons to the js file

--- BoardGamesShop/BoardGamesShop/wwwroot/js/allGames.js
+++ BoardGamesShop/BoardGamesShop/wwwroot/js/allGames.js
@@ -1,6 +1,17 @@
 const clearButton = document.querySelector(".controls > button:nth-child(2)");
-const selectedInputs = document.querySelectorAll(".offcanvas-body input[type=checkbox]:checked");
+const selectedInputs = 
+    document.querySelectorAll(".offcanvas-body input[type=checkbox]:checked");
 clearButton.addEventListener("click", function (e) {
     selectedInputs.forEach((selectedInput) => selectedInput.checked = false);
     e.target.closest(form).submit();
+});
+
+document.querySelector("button#prev-page-btn").addEventListener("click", function () {
+    let currentPageVal = parseInt(document.querySelector("#CurrentPage").value);
+    document.querySelector('#CurrentPage').value = currentPageVal - 1;
+});
+
+document.querySelector("button#next-page-btn").addEventListener("click", function () {
+    let currentPageVal = parseInt(document.querySelector("#CurrentPage").value);
+    document.querySelector('#CurrentPage').value = currentPageVal + 1;
 });
